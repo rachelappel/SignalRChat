18e0ef65095fc0b85e8e23201431863e422a799e
rachelappel <rachelap@microsoft.com>
2018-04-04 19:18:08 -0400

    Add project files.

---
+++ SignalRChat/wwwroot/js/chat.js
@@ -0,0 +1,16 @@
+﻿const connection = new signalR.HubConnection("/chathub");
+
+connection.on("ReceiveMessage", (message) => {
+    const encodedMsg = message;
+    const li = document.createElement("li");
+    li.innerText = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+document.getElementById("sendButton").addEventListener("click", event => {
+     const message = document.getElementById("messageInput").value;
+     connection.send("SendMessage", message).catch(err => console.error);
+     event.preventDefault();
+});
+
+connection.start().catch(err => console.error);
