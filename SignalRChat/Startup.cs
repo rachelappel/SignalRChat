8c0825e81b1f2aa1e9dbbdcbd3eacda383135d73
rachelappel <rachelap@microsoft.com>
2018-04-27 14:26:43 -0400

    Update for ASP.NET Core 2.1.0 RC1

--- SignalRChat/Startup.cs
+++ SignalRChat/Startup.cs
@@ -1,6 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SignalRChat.Hubs;
@@ -16,31 +22,26 @@ namespace SignalRChat
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddMvc();
 
-            //services.AddCors(options => options.AddPolicy("CorsPolicy", 
-            //builder => 
-            //{
-            //    builder.AllowAnyMethod().AllowAnyHeader()
-            //           .WithOrigins("http://localhost:55830");
-            //}));
-
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddSignalR();
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
-                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
             }
             else
@@ -52,12 +53,12 @@ namespace SignalRChat
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-            //app.UseCors("CorsPolicy");
-            app.UseSignalR(routes => 
+
+            app.UseSignalR(route =>
             {
-                routes.MapHub<ChatHub>("/chathub");
+                route.MapHub<ChatHub>("/chathub");
             });
-            app.UseMvc();            
+            app.UseMvc();
         }
     }
 }
