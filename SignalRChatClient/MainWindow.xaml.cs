b47ddbb7f6eddb3d9428dd19332ae67e9bdb5e3e
rachelappel <rachelap@microsoft.com>
2018-04-27 15:23:10 -0400

    SignalR Chat WPF .NET Client

---
+++ SignalRChatClient/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Windows;
+
+namespace SignalRChatClient
+{
+    public partial class MainWindow : Window
+    {
+        static HubConnection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+            connection = new HubConnectionBuilder()
+            .WithUrl("https://localhost:44317/chathub")
+            .Build();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            connection.On<string>("ReceiveMessage", data =>
+            {
+                Console.WriteLine($"Received: {data}");
+            });
+
+            try
+            {
+                await connection.StartAsync();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                await connection.InvokeAsync("SendMessage", "Hello there");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                throw;
+            }
+            
+        }
+    }
+}
